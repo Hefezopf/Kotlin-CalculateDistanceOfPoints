181a4ed3c0ddb4ae2e60a1eab8a437990b117212
Hefezopf <markus@hopf-it.de>
2020-05-02 17:42:51 +0200

    math

--- src/org/Point.kt
+++ src/org/Point.kt
@@ -1,14 +1,17 @@
 package org
 
-class Point (var x: Int, var y: Int) {
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class Point (var x: Int, var y: Int) {
 }
 
 fun determineMinimum(p1: Point, p2: Point): Double {
-    return Math.sqrt(Math.pow((p2.x - p1.x).toDouble(), 2.0) + Math.pow((p2.y - p1.y).toDouble(), 2.0))
+    return sqrt((p2.x - p1.x).toDouble().pow(2.0) + (p2.y - p1.y).toDouble().pow(2.0))
 }
 
 fun determineMinimumOfArray(points: Array<Point>): Double {
-    require(points.size >= 1) { "too less points" }
+    require(points.isNotEmpty()) { "too less points" }
     var min = Double.POSITIVE_INFINITY
     for (i in points.indices) {
         val startPoint = points[i]
@@ -17,8 +20,8 @@ fun determineMinimumOfArray(points: Array<Point>): Double {
     return min
 }
 
-private fun calculate(points: Array<Point>, startPoint: Point, min: Double, startVal: Int): Double {
-    var min = min
+private fun calculate(points: Array<Point>, startPoint: Point, minimum: Double, startVal: Int): Double {
+    var min = minimum
     for (i in startVal + 1 until points.size) {
         val minTemp = determineMinimum(startPoint, points[i])
         if (minTemp < min) {
