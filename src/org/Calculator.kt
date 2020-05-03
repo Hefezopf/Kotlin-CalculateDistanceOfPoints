4ed335febd02960013407719fab7a5dbb5b0df88
Hefezopf <markus@hopf-it.de>
2020-05-03 13:10:22 +0200

    more

--- src/org/Calculator.kt
+++ src/org/Calculator.kt
@@ -5,10 +5,10 @@ class Calculator {
 
 fun main(args: Array<String>) {
     println("Start!!!!!!")
-    val p1 = Point(3, 4)
-    val p2 = Point(4, 4)
-    val p3 = Point(5, 2)
-    val p4 = Point(6, 7)
+    val p1 = Point(1, 4)
+    val p2 = Point(2, 3)
+    val p3 = Point(3, 2)
+    val p4 = Point(4, 1)
     val points = arrayOf(p1, p2, p3, p4)
     val minDistance = determineMinimum(p1, p2)
     println("Distance:$minDistance")
